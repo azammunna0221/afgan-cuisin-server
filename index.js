79332c64be60ca0a7327c7c021f03a67e9ed8d8d
Ali Azam <azammunna0221>
2023-05-05 15:03:36 +0600

    Router chef link ID added

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const app = express()
 const port = 5000
 
 app.use(cors());
+app.get('/chefInfo/:id' , (req, res) => {
+  const id = req.params.id;
+  const selectedData = chefData.find(d => d.id == id); 
+  res.send(selectedData)
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -14,6 +19,7 @@ app.get('/', (req, res) => {
 app.get('/data', (req, res) => {
   res.send(chefData)
 })
+
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
